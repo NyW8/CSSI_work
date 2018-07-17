d79f3a1b47949ba2060b03327850bf4543e3c6dd
Demo User <demouser@MacBook-Pro.local>
2018-07-17 16:02:21 -0500

    Half finished work here...

--- javaScript/labs/Dog_Walker/script.js
+++ javaScript/labs/Dog_Walker/script.js
@@ -11,12 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+function printer(name, time, fav=false){
+  if (!fav){
+    console.log("I will walk "+name+" today at "+time);
+  }else{
+    console.log("I will walk "+name+" one of my favorite dogs, today at "+time);
+  }
+}
 
+let favDogs = [Spike, Jeremy, Lola, Peaches, Steve];
 // Task 1
 let dogName1 = "Steve";
 let dogType1 = "beagle";
 
 // Complete Task 1 Below
+printer(dogName1, "12:00 pm");
+
 
 
 
@@ -25,9 +35,40 @@ let dogType2 = "bulldog";
 
 // Complete Task 2 Below
 
+if (dogType2 == "corgi"){
+  printer(dogName2, "12:00 pm");
+} else {
+  printer(dogName2, "1:00 pm");
+}
 
 
 let dogName = "Lola";
 let dogType = "poodle";
 
 // Complete Task 3 Below
+if (dogType == "corgi" || dogType == "beagle"){
+  printer(dogName, "1:00 pm");
+} else {
+  printer(dogName, "2:00 pm");
+}
+
+function test(name2, type){
+  //SWITCH NAME2 TO NAME
+  let found = false;
+  for (var v in favDogs) {
+    if (v == name) {
+      if (type== "corgi" || type== "beagle"){
+        printer(name, "1:00 pm", true);
+      } else {
+        printer(name, "2:00 pm", true);
+      }
+      found = true;
+      break;
+    }
+  }
+  if (!found && (type == "corgi" || type == "beagle")){
+            printer(name, "1:00 pm");
+          } else {
+            printer(name, "2:00 pm");
+          }
+}
