7b282f72c0e4aecd084c6a0942701805e12af971
Demo User <demouser@MacBook-Pro.local>
2018-07-18 12:04:36 -0500

    Finished task

--- javaScript/labs/Loop_De_Loop/script.js
+++ javaScript/labs/Loop_De_Loop/script.js
@@ -14,14 +14,26 @@
 
 let amusementParks = ["Disney World", "Hershey Park", "Six Flags", "Dorney Park", "Sea World"];
 //Complete task one below
-
+amusementParks.forEach((name) => {
+  console.log(name.toUpperCase()+" is a great amusement park.");
+});
 
 
 
 let rollerCoasters = ["Big Thunder Mountain Railroad","Splash Mountain","Space Mountain", "Expedition Everest","The Twilight Zone Tower of Terror"];
 //Complete task two below
-
+for (let i = 0; i < rollerCoasters.length; i++){
+  console.log("I will ride "+rollerCoasters[i]);
+  if (rollerCoasters[i] == "Space Mountain"){
+    break;
+  }
+}
 
 
 let height = 6;
 //Complete task three below
+
+while(height > 4){
+  console.log("Next Customer");
+  height = Math.floor(Math.random()*5)+2;
+}
