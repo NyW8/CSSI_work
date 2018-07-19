3a25052650db792a0e0f32c14ac949072a76ead9
Demo User <demouser@MacBook-Pro.local>
2018-07-19 15:56:30 -0500

    Created shopping list python program

--- python/labs/shopping-list-app/starter-code/shopping_list_starter.py
+++ python/labs/shopping-list-app/starter-code/shopping_list_starter.py
@@ -31,3 +31,34 @@ while choice.lower() != "e":
     choice = input("Enter your choice [a|b|c|d|e]:")
     
     # Your code below! Handle the cases when the user chooses a, b, c, d, or e
+
+    if choice == "a":
+        adder = input("What would you like to add? Separate values with commas: ").lower()
+        adder = adder.split(",")
+        for add in adder:
+            if add in shopping_list:
+                print(add+" is already on the list!")
+            else:
+                shopping_list.append(add)
+                print(add+" has been added to the list.")
+    elif choice == "b":
+        remover = input ("What would you like to remove? ").lower()
+        if (remover in shopping_list):
+            a = input("Are you sure you would like to remove "+remover+" y/n ")
+            if a =="y":
+                shopping_list.remove(remover)
+            else:
+                print("Cancelled")
+        else:
+            print("I'm sorry, that was not found in the list.")
+    elif choice == "c":
+        checker = input("What would you like to check for? ").lower()
+        if checker in shopping_list:
+            print("That item is in your list already!")
+        else:
+            a = input("That item is not currently in your list, would you like to add "+checker+" y/n")
+            if a =="y":
+                shopping_list.append(checker)
+    elif choice == "d":
+        for thing in shopping_list:
+            print (thing)
