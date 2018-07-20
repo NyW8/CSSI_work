9ca97c1d1795ee5213f787fcb1b40e058802ad22
Demo User <demouser@MacBook-Pro.local>
2018-07-20 12:06:02 -0500

    Finished short madlibs lab

---
+++ python/labs/make-a-short-story/mystory.py
@@ -0,0 +1,7 @@
+
+n1 = input("Noun: ")
+n2 = input("Another noun: ")
+a1 = input("Adjective: ")
+v1 = input("Verb ending in -ing: ")
+generic = "The "+n1+" jumped over a "+a1+" "+n2+".  Then the "+n2+" decided to stop being so "+a1+" and take up a hobby: "+v1
+print generic
