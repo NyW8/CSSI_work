6d5e60b3e9e13150c21bdda27524192c228ce645
Demo User <demouser@MacBook-Pro.local>
2018-07-23 08:59:46 -0500

    Random tests in Python

--- python/pytest.py
+++ python/pytest.py
@@ -1,7 +1,18 @@
-x = 20
-if  x > 18 and x < 21:
-    print ("cat can vote but not drink")
-elif x > 18:
-    print ("cat can vote")
-else:
-    print ("cat cannot vote")
+from datetime import date
+class Person(object):
+    def __init__(self, yearBorn, live, hair):
+        currentYear = int(date.today().year)
+        self.born = yearBorn
+        self.live = live
+        self.hair = hair.lower()
+        self.age = currentYear - yearBorn
+        self.adult = self.age >= 18
+    def __str__(self):
+        return ("age: "+str(self.age)+" live: "+self.live+" hair: "+self.hair+" adult: "+str(self.adult))
+    def code(self):
+        return ("Hello World!")
+
+nyah = Person(2000, "Ontario, Canada", "brown")
+
+print nyah
+print nyah.code()
