2824de8584c4d85f9efc227a0b7effe01fc15d2a
Demo User <demouser@MacBook-Pro.local>
2018-07-24 11:26:53 -0500

    Added documentation and passing data to html

--- Meme_Maker/main.py
+++ Meme_Maker/main.py
@@ -12,37 +12,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Import statements! Will be using Google's app engine
 import webapp2
 from google.appengine.api import urlfetch
 import json
 import random
 import urllib
+import jinja2
 
 
-class MainPage(webapp2.RequestHandler):
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader = templateLoader)
+
+class MemePage(webapp2.RequestHandler):
+    """ A meme generator page, generates a random meme picture with
+        a random caption or specified captions through the url
+    """
+
     def get(self):
+        # text1 and text2 can be specified through the url
         text1 = self.request.get("text1")
         text2 = self.request.get("text2")
         self.response.headers['Content-Type'] = 'text/html'
-        #self.response.write('Hello, Programmers!')
-        #result = requests.get('https://api.imgflip.com/get_memes')
-        #self.response.write('Hello, Programmers! '+result)
-        #self.
+
+        # Our api url
         url = 'https://api.imgflip.com/get_memes'
+
+        #Try to access the internet to make a meme
         try:
             result = urlfetch.fetch(url)
+
+            #Random top and bottom text in case one or both is not specified
             random1=["Sand", "Fruit Juice", "Programming", "My Oyster", "Kleenex", "Google?", "hamburgers", "and then i said"]
             random2=["is delicious", "sucks", "grows on trees", "needs to be fired", "My oyster", "Rocks", "for the win"]
+
+            #Check that site could be reached
             if result.status_code == 200:
-                #self.response.write(result.content)
+
+                #Change result.content from string to dictuionary and get a meme dictionary
                 memeDictionary = json.loads(result.content)
                 memes = memeDictionary["data"]["memes"]
+
+                #Choose random meme and get the url of it
                 url = random.choice(memes)["url"]
                 caption_url = 'https://api.imgflip.com/caption_image'
+
+                #Assign text if not already assigned
                 if text1=="":
                     text1=random.choice(random1)
                 if text2=="":
                     text2=random.choice(random2)
+
+                #A dictionary to send to imgflip with all the data
                 captionDictionary = {
                     "template_id":memes[random.randint(1,100)]["id"],
                     "username":"ded0fd30",
@@ -50,28 +71,47 @@ class MainPage(webapp2.RequestHandler):
                     "text0":text1,
                     "text1":text2,
                 }
+
+                #Use post to return img url
                 result= urlfetch.fetch(
                     url=caption_url,
                     payload=urllib.urlencode(captionDictionary),
                     method=urlfetch.POST,)
                 meemUrl = json.loads(result.content)["data"]["url"]
-                self.response.write("<html><img src="+meemUrl+"></html>")
 
+                #Add image to webpage
+                self.response.write("<html><img src="+meemUrl+"></html>")
+            #Set status code if not successful
             else:
                 self.response.status_code = result.status_code
 
+        #If connection doesn't work, log an error
         except urlfetch.Error:
             logging.exception("Caught error")
 
+class MainPage(webapp2.RequestHandler):
+    """Shows the main (home) page """
+    def get(self):
+        template = templateEnv.get_template('/home.html')
+        #home_html=open('home.html').read()
+        self.response.write(template.render())
 
+class Recipe(webapp2.RequestHandler):
+    """Shows a recipe html page"""
+    def get(self):
+        template = templateEnv.get_template('/recipe.html')
+        recipe = {"ingredients": ["cottage cheese", "pineapple"], "cuisine":"nixonian"}
+        self.response.write(template.render(recipe))
 
 class FourOhFour(webapp2.RequestHandler):
+    """ Manual 404 page not found request handler
+    """
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('<html><h1>404 Error: Page Not Found!</h1><body>We could not find the page you were looking for</body></html>')
-        #self.response.write('404 Error: Page Not Found')
 
 app = webapp2.WSGIApplication([
-    ('/memes.*', MainPage),
+    ('/memes.*', MemePage),
+    ('/home', MainPage),
+    ('/recipies', Recipe),
     ('/.*', FourOhFour)
 ], debug=True)
